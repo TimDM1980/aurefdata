6a2ef89272ec42cd06fbd2a61778fda7b6ce3f58
bktid <Cegeka01>
2017-02-14 20:42:50 +0100

    stubserver cors

--- stubserver/server.js
+++ stubserver/server.js
@@ -3,7 +3,14 @@ const _ = require('lodash');
 const path = require('path');
 
 const app = express();
-app.get('/kantoor', (req, res) => {
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/lookup/kantoor', (req, res) => {
   let kantoor = {
     code: "123"
   };
